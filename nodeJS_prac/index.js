160a708134ed38811db0d45472839cd0102e8c60
Skaps664 <msudaisk664@gmail.com>
2023-06-27 15:37:12 +0500

    Server reads and sends JSON

--- nodeJS_prac/index.js
+++ nodeJS_prac/index.js
@@ -37,6 +37,10 @@ const url = require("url");
 
 //-------
 //SREVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -44,7 +48,15 @@ const server = http.createServer((req, res) => {
     res.end("This is the overview");
   } else if (pathName == "/product") {
     res.end("This is the product page");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
   } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
     res.end("Page not found");
   }
 });
