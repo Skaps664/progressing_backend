5af7b7d033e8efafaec89363163d671a985c10f8
Skaps664 <msudaisk664@gmail.com>
2023-07-11 14:18:37 +0500

    express updated

--- express_prac/app.js
+++ express_prac/app.js
@@ -5,26 +5,15 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the serverside', app: 'Notours' });
-// });
-
-// app.post('/', () => {
-//   res.send('You can post to this endpoint');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({ status: 'success', data: { tours } });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  // since we dont have database, so to add new item to last id+1
+};
+const postTour = (req, res) => {
+  // since we don't have database, so to add new item to last id+1
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -33,7 +22,7 @@ app.post('/api/v1/tours', (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      res.status(200).json({
+      res.status(201).json({
         status: 'success',
         data: {
           tour: newTour,
@@ -41,9 +30,61 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+};
+const getTourByID = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  } else {
+    res.status(200).json({ status: 'success', data: { tour } });
+  }
+};
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.id.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  } else {
+    res
+      .status(200)
+      .json({ status: 'success', data: { tour: '<updated tour here..>' } });
+  }
+};
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.id.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  } else {
+    res.status(204).json({ status: 'success', data: null });
+  }
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', postTour);
+// app.get('/api/v1/tours/:id', getTourByID);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(postTour);
+app
+  .route('api/v1/tours/:id')
+  .get(getTourByID)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.use((req, res) => {
+  res.send('<h1>404! Not Found<h1>');
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App runnning on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
