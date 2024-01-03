8cc54ef652617483fb1b515e38e24b7d4e8ecbd0
Skaps664 <msudaisk664@gmail.com>
2024-01-03 12:03:12 +0500

    settingup mongo

--- backend_dev3/routes/index.js
+++ backend_dev3/routes/index.js
@@ -1,9 +1,22 @@
 var express = require("express");
+
+const userModel = require("../models/userModel");
+
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index");
 });
+
+// router.get("/create", async function (req, res) {
+//   let userData = await userModel.create({
+//     username: "Sudais",
+//     nickname: "skaps",
+//     description: "3rd year CS undergrad",
+//     categories: ["art", "backend", "football"],
+//   });
+//   res.send(userData);
+// });
 
 module.exports = router;
