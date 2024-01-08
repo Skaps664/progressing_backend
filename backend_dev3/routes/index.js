71979b69a0dd3d33b578975b15bc3ad5f35960ec
Skaps664 <msudaisk664@gmail.com>
2024-01-08 18:05:36 +0500

    Added some mongo

--- backend_dev3/routes/index.js
+++ backend_dev3/routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-const userModel = require("../models/userModel");
+const userModel = require("./users");
 
 var router = express.Router();
 
@@ -9,14 +9,14 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
-// router.get("/create", async function (req, res) {
-//   let userData = await userModel.create({
-//     username: "Sudais",
-//     nickname: "skaps",
-//     description: "3rd year CS undergrad",
-//     categories: ["art", "backend", "football"],
-//   });
-//   res.send(userData);
-// });
+router.get("/create", async function (req, res) {
+  let userData = await userModel.create({
+    username: "Sudais",
+    nickname: "skaps",
+    description: "3rd year CS undergrad",
+    categories: ["art", "backend", "football"],
+  });
+  res.send(userData);
+});
 
 module.exports = router;
